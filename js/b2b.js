d593be7ef9fee1354ab65e52fc1ff72c889aac7a
louie-espinosa <louie.espinosa@proton.me>
2023-03-07 17:15:44 -0600

    Added a button that toggles a red dotted border on all divs

--- js/b2b.js
+++ js/b2b.js
@@ -1,6 +1,10 @@
-function click2Win() {
-    let bugButton = document.querySelector("bugs");
-    bugButton.addEventListener("click", function() {
+let bugButton = document.querySelector(".bugs");
+    
+bugButton.addEventListener("click", function() {
+    const collection = document.body.children;
+    for (let i = 0; i < collection.length; i++) {
+       
+        collection[i].classList.toggle("red-bor");
         
-    });
-}
+    }
+});
