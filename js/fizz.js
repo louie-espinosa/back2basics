a9758634a1d55cf6a66960d564743649b39fc36d
louie-espinosa <louie.espinosa@proton.me>
2023-03-09 23:08:55 -0600

    Successfully created the fizzbuzz function w/ js

--- js/fizz.js
+++ js/fizz.js
@@ -2,8 +2,15 @@
 
 let text = "";
 
-for (let i = 0; i < 100; i++) {
-    text = [i];
-
-    
+for (let i = 1; i <= 100; i++) {
+   
+    if(i % 15 == 0) {
+        console.log("fizzbuzz");
+    } else if (i % 5 == 0) {
+        console.log("buzz");
+    } else if (i % 3 == 0) {
+        console.log("fizz"); 
+    } else {
+        console.log(i);
+    }
 }
