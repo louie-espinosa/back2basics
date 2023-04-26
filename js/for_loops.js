d317bc8a5765e00f4792a78100a8c7682b95cf23
louie-espinosa <louie.espinosa@proton.me>
2023-04-25 23:52:21 -0500

    Created function with for loop and conditions, but incomplete ex.2 in for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,10 +1,26 @@
 "use strict";
 
-//Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+//1. Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
 function showMultiplicationTable(x) {
     for (let i = 1; i <= 10; i++) {
-        console.log('The number ' + x + ' times ' + i + ' = ' + (x*i))
+        console.log( x + ' x ' + i + ' = ' + (x*i))
     }
-   
 }
-showMultiplicationTable(10)
+//showMultiplicationTable(10)
+
+//2. Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.
+let luckyNumber = Math.floor(Math.random() * 182) + 19;
+
+function randomTwentyToTwoHundred() {
+  
+    for (let i = 1; i <= 10; i++) {
+        console.log(luckyNumber)
+        if(Math.abs(luckyNumber[i] % 2) === 1) {
+            console.log( luckyNumber[i] + ' is odd')
+        } else {
+            console.log( luckyNumber[i] + ' is even')
+        }
+       
+    }
+}
+randomTwentyToTwoHundred();
