af37598b37968bc899698be965a3e34e46adc6e2
louie-espinosa <louie.espinosa@proton.me>
2023-04-25 23:18:03 -0500

    Completed ex.1 in for loops.

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("Hello")
+
+//Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+function showMultiplicationTable(x) {
+    for (let i = 1; i <= 10; i++) {
+        console.log('The number ' + x + ' times ' + i + ' = ' + (x*i))
+    }
+   
+}
+showMultiplicationTable(10)
