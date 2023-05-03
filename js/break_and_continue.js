8e3d3b3472beae422fb1725a5700a923367a83eb
louie-espinosa <louie.espinosa@proton.me>
2023-05-02 21:18:01 -0500

    Created prompt, for loop, and conditional for ex 1, break and continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,1 +1,20 @@
 "use strict";
+//BREAK AND CONTINUE
+//1. Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+//Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+let userOddNumber = prompt('Enter an odd number between 1 and 50: ');
+
+let output = ""
+for (let i = 0; i <= 50; i++) {
+    if (Math.abs(userOddNumber % 2) !== 1 || isNaN(userOddNumber) || userOddNumber < 1 || userOddNumber > 50) {
+        
+        let UserOddNumber = prompt('You did not enter an odd number. Please enter an odd number between 1 and 50: ');
+        break;
+
+    } else {
+        output += i;
+        continue;
+    }
+}
+
