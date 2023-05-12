f9a897ea52325552e47bed64aebf932779b782aa
louie-espinosa <louie.espinosa@proton.me>
2023-05-11 22:38:35 -0500

    Completed planets_array exercise to refresh on js array manipulation

--- js/planets_array.js
+++ js/planets_array.js
@@ -20,21 +20,23 @@
 planets.unshift('The Sun')
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
-
+planets.push('Pluto');
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
-
+planets.shift();
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
-
+planets.pop();
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
-
+console.log('The index of the Earth is: ' + planets.indexOf('Earth'))
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
 
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
 })();
