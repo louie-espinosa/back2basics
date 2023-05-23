4e7bed3f2b50471cb72fdf13fe95be8b00527307
louie-espinosa <louie.espinosa@proton.me>
2023-05-23 18:42:22 -0500

    Completed circle object exercises to practice manipulating object with Math.round

--- js/circle.js
+++ js/circle.js
@@ -2,23 +2,26 @@
     "use strict";
 
     // create a circle object
-    var circle = {
+    let circle = {
         radius: 3,
 
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            let pi = Math.PI
+            let area = pi * (this.radius**2)
+            return area;
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
+            if (doRounding) {
+                console.log(Math.round(this.getArea()))
+            } else 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
         }
     };
 
@@ -30,10 +33,12 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
-    circle.logInfo(false);
+    
+    circle.logInfo(false)
+    
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
 })();
